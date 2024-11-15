8ed9a38f63fcc8e2e0cf26915e8d9a8a3bce5bcd
abhisekray978 <abhisekray978@gmail.com>
2024-11-15 10:00:29 +0545

    Add files via upload

---
+++ DriverManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Railway;
+
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author HP
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcmysqllocalhostrailwayreservation, String root, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
